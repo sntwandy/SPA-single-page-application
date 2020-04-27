c3e2b4787320582fcc60f73eea748f07a80879a5
sntwandy <sntwandy@gmail.com>
2020-04-27 08:56:14 -0400

    Config resolveRoutes and getHash

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import Header from '../templates/header';
 import Home from '../pages/home';
 import Character from '../pages/character';
 import Error404 from '../pages/error404';
+import getHash from '../utils/getHash';
+import resolveRoutes from '../utils/resolveRoutes';
 
 // Routes
 const routes = {
