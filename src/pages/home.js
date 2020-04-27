55a7dec2a90d81855d4b32588ed7678b04da4e58
sntwandy <sntwandy@gmail.com>
2020-04-27 11:46:34 -0400

    Making petitions to API and rendering

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,17 @@
-const home = () => {
+import getData from '../utils/getData';
+
+const home = async () => {
+const characters = await getData();
     const view = `
         <div class="characters">
-            <article class="character-item">
-                <a href="#/1/">
-                    <img src="image" alt="name" />
-                    <h2>Name</h2>
-                </a>
-            </article>
+            ${characters.results.map(character => `
+                <article class="character-item">
+                    <a href="#/${character.id}/">
+                        <img src="${character.image}" alt="${character.name}" />
+                        <h2>${character.name}</h2>
+                    </a>
+                </article>
+            `).join('')}
         </div>
     `;
     return view;
