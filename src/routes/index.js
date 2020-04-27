399fcdd13beb6213bf9f79f94a93bdb05f131a4e
sntwandy <sntwandy@gmail.com>
2020-04-27 09:24:35 -0400

    Finishing the router and routes config

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import Character from '../pages/character';
 import Error404 from '../pages/error404';
 import getHash from '../utils/getHash';
 import resolveRoutes from '../utils/resolveRoutes';
+import error404 from '../pages/error404';
 
 // Routes
 const routes = {
@@ -21,6 +22,8 @@ const router = async () => {
 
     let hash = getHash();
     let route = await resolveRoutes(hash);
+    let render = routes[route] ? routes[route] : Error404;
+    content.innerHTML = await render();
 };
 
 export default router;
