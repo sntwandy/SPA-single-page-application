1b2b3fd957296638cbd71451491d3d0b11b987ae
sntwandy <sntwandy@gmail.com>
2020-04-28 04:27:25 -0400

    Adding styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import router from './routes';
 
+const checkbox = document.getElementById("checkbox");
+
 window.addEventListener('load', router);
 window.addEventListener('hashchange', router);
+
+checkbox.addEventListener('click', (event) =>
+    document.body.classList.toggle('dark')
+);
